665994478d6cb44feaff272198a3f10c4b25d0bd
MosecX <111986841+MosecX@users.noreply.github.com>
2025-12-04 23:40:38 +0000

    1.0.0.3

--- app/api/account/route.ts
+++ app/api/account/route.ts
@@ -5,7 +5,7 @@ import { cookies } from "next/headers";
 
 export async function GET() {
   try {
-    const cookieStore = wawit cookies(); // ✅ es síncrono, no uses await
+    const cookieStore = await cookies(); // ✅ es síncrono, no uses await
     const token = cookieStore.get("token")?.value;
 
     if (!token) {
